368d05be910df3055df125955d844ae250a73dbb
Coen Bernaards <cab.gparotation@gmail.com>
2022-10-22 07:47:50 +0000

    version 2022.10-2

--- inst/doc/Guide.R
+++ inst/doc/Guide.R
@@ -1,19 +1,19 @@
 ### R code from vignette source 'Guide.Stex'
 
 ###################################################
-### code chunk number 1: Guide.Stex:6-7
+### code chunk number 1: Guide.Stex:7-8
 ###################################################
  options(continue="  ")
 
 
 ###################################################
-### code chunk number 2: Guide.Stex:12-13
+### code chunk number 2: Guide.Stex:13-14
 ###################################################
 library("GPArotation")  
 
 
 ###################################################
-### code chunk number 3: Guide.Stex:27-30
+### code chunk number 3: Guide.Stex:30-33
 ###################################################
   data(ability.cov)
   z <- factanal(factors = 2, covmat = ability.cov, rotation="oblimin")
